740a7fb361744fe7c7e670553cd05f991c8fafcc
Prajwal Patil <prajwalpatil852@gmail.com>
2022-07-24 13:21:56 +0530

    Add files via upload

---
+++ Hollow.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	int l, b;
+	cout<<"Enter the length";
+	cin>>l;
+	cout<<"Enter the breadth";
+	cin>>b;
+	
+	for(int i=1; i<=b; i++)
+	{
+		for(int j=1; j<=l; j++)
+		{
+			if(i==1 || i==b || j==1 || j==l)
+			{
+				cout<<"*";
+			}
+			else
+			{
+				cout<<" ";
+			}
+		}
+		cout<<"\n";
+	}
+}
